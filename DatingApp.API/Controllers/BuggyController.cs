8afbfc761745fcfa02704afba7f9488577ae6246
k-mariana <mariana.karabin3@gmail.com>
2025-03-20 21:57:20 +0200

    Add error handling

---
+++ DatingApp.API/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers;
+
+public class BuggyController(DataContext context): BaseApiController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secret text";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var thing = context.Users.Find(Guid.Parse("00000000-0000-0000-0000-000000000000"));
+        if (thing == null) return NotFound();
+        return thing;
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError()
+    {
+        var thing = context.Users.Find(Guid.Parse("00000000-0000-0000-0000-000000000000")) ?? throw new Exception("A bad thing has happened");
+        return thing;
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+}
--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -1,4 +1,5 @@
 using DatingApp.API.Extensions;
+using DatingApp.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +21,7 @@ if (app.Environment.IsDevelopment())
     });
     //app.MapOpenApi();
 }
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection(); 
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
