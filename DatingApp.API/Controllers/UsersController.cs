00b6c9ee9045c8a463825a7339b351a927935d4c
k-mariana <mariana.karabin3@gmail.com>
2025-03-25 18:42:14 +0200

    Updating resources

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -6,11 +6,12 @@ using DatingApp.API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace DatingApp.API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository): BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper): BaseApiController
 {
 
     
@@ -34,5 +35,18 @@ public class UsersController(IUserRepository userRepository): BaseApiController
         }
         
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null) return BadRequest("No username found in token");
+        var user = await userRepository.GetUserByUsernameAsync(username);
+        if (user == null) return BadRequest("Could not find user");
+        mapper.Map(memberUpdateDto, user);
+        
+        if (await userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update the user");
     }
 }
